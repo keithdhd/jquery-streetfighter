79894dcbe9df7ee78acd3832384b47d6c40e5aa8
keithdhd <keith@keithdouglas.co.uk>
2015-03-30 18:07:53 +0100

    Added audio commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 //After page load, display standing still image of Ryu
 $(document).ready(function(){
-	var ryu = $(".ryu img");
+	init();
+	playGame();
+});	
 
+function init(){
+	$("#theme-song").trigger("play");
+	$(".text-content .instructions").fadeIn(1000);
+}
+
+
+function playGame(){
+	var ryu = $(".ryu img");
+	var hadouken = $(".hadouken");
+	
 	//ryu.onHover turn him into a "ready" image
 	ryu.hover(function(){
     	$(this).attr("src", "images/ryu-ready-position.gif");
@@ -12,22 +24,51 @@ $(document).ready(function(){
 	//ryu.onClick Ryu fires a Hadouken
 	ryu.mousedown(function() {
  	 $(this).attr("src", "images/ryu-throwing-hadouken.png");
+ 	 
  	 //play the sound
- 	 $("#audioFile").trigger('play');
+ 	 $("#hadouken-sound").trigger("play");
+ 	 
+ 	 //fire hadouken
+		hadouken.show().animate(
+		  {'left': '1020px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '520px');
+		  }
+		);
+
 	});
 	
 	//ryu.onClick release, return to "ready" pose
 	ryu.mouseup(function() {
  	 $(this).attr("src", "images/ryu-ready-position.gif");
 	});
+	
+	//x key.onkeyDown Ryu looks cool
+	$(document).keydown(
+	function( event ) {
+  	if ( event.which == 88 ) {
+     ryu.attr("src", "images/ryu-cool.gif");
+     $("#coolMusic").trigger('play');
+ 	}
+  	});
 
-})
+  	//x key.onKeyUp Ryu is "ready"
+  	$(document).keyup(
+	function( event ) {
+  	if ( event.which == 88 ) {
+     ryu.attr("src", "images/ryu-standing-still.png");
+ 	}
+  	});
+
+}
 
 
 
 
 
 
-//x key.onkeyDown Ryu looks cool
 
-//x key.onKeyUp Ryu is "ready"
+
+
